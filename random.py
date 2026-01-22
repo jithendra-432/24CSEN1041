92496e51e3478d792086b2077a825649ed667cd1
jithendra-432 <ddandu@student.gitam.edu>
2026-01-22 15:32:34 +0530

    Create random.py

---
+++ random.py
@@ -0,0 +1,11 @@
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.randint(0,9))
+
+#output 
+3
+
+it create a random number in between 0 to 9
