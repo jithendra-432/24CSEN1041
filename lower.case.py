8dee59ee1580efc7ca877d5110fc9e9cb009790c
jithendra-432 <ddandu@student.gitam.edu>
2026-01-28 11:36:05 +0530

    Create lower.case.py

---
+++ lower.case.py
@@ -0,0 +1,13 @@
+shopping_list = ["APPlES", "BANANAS", "dill pickle", "eggplant"]
+
+print("Shopping List Items:")
+for item in shopping_list:
+    print(f"- {item.lower()}")
+  ##output
+  Shopping List Items:
+- apples
+- bananas
+- dill pickle
+- eggplant
+
+   
