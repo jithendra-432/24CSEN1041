02dd316a6289570772a5db05d22c029cb04a3bec
jithendra-432 <ddandu@student.gitam.edu>
2026-01-28 11:26:35 +0530

    Create capital.py

---
+++ capital.py
@@ -0,0 +1,27 @@
+shopping_list = ["apples", "bananas", "carrots", "dill pickle", "eggplant"]
+
+print("Shopping List Items:")
+for item in shopping_list:
+    print(f"- {item.capitalize()}")
+ #3output   
+
+   Shopping List Items:
+- Apples
+- Bananas
+- Carrots
+- Dill pickle
+- Eggplant
+
+       shopping_list = ["apples", "bananas", "carrots", "dill pickle", "eggplant"]
+
+print("Shopping List Items:")
+for item in shopping_list:
+    print(f"- {item.upper()}")
+ ##output
+   Shopping List Items:
+- APPLES
+- BANANAS
+- CARROTS
+- DILL PICKLE
+- EGGPLANT
+
