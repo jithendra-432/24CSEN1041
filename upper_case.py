b17d3f1ee93b8bb9f05eab0947f394c0cac89599
jithendra-432 <ddandu@student.gitam.edu>
2026-01-28 11:38:29 +0530

    Create upper_case.py

---
+++ upper_case.py
@@ -0,0 +1,12 @@
+ shopping_list = ["apples", "bananas", "carrots", "dill pickle", "eggplant"]
+
+print("Shopping List Items:")
+for item in shopping_list:
+    print(f"- {item.upper()}")
+ ##output
+   Shopping List Items:
+- APPLES
+- BANANAS
+- CARROTS
+- DILL PICKLE
+- EGGPLANT
