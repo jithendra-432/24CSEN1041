12223873367f86407cfa1311680a8ece804262af
jithendra-432 <ddandu@student.gitam.edu>
2026-01-29 15:28:31 +0530

    Create logical _operation.py

---
+++ logical _operation.py
@@ -0,0 +1,18 @@
+print("=== Logical Operations ===")
+p = True
+q = False
+
+print("p =", p, "q =", q)
+print("p and q :", p and q)  # True only if both are True
+print("p or q  :", p or q)   # True if at least one is True
+print("not p   :", not p)    # inverts True/False
+
+
+##output
+  === Logical Operations ===
+p = True q = False
+p and q : False
+p or q  : True
+not p   : False
+
+=== Code Execution Successful ===
