fdeb55460868225a06d061efa538bf4a5ce41c8d
jithendra-432 <ddandu@student.gitam.edu>
2026-01-29 15:12:28 +0530

    Create relative_operation.py

---
+++ relative_operation.py
@@ -0,0 +1,22 @@
+print("=== Comparison Operations ===")
+x = 10
+y = 20
+
+print("x =", x, "y =", y)
+print("x == y  :", x == y)   # equal
+print("x != y  :", x != y)   # not equal
+print("x > y   :", x > y)
+print("x < y   :", x < y)
+print("x >= y  :", x >= y)
+print("x <= y  :", x <= y)
+ ##output 
+=== Comparison Operations ===
+x = 10 y = 20
+x == y  : False
+x != y  : True
+x > y   : False
+x < y   : True
+x >= y  : False
+x <= y  : True
+
+=== Code Execution Successful ===
