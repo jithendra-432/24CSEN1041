79badb42caffcc91b109ee8c6adca8cd26b30feb
jithendra-432 <ddandu@student.gitam.edu>
2026-01-29 15:42:06 +0530

    Create identity_operation.py

---
+++ identity_operation.py
@@ -0,0 +1,17 @@
+print("=== Identity Operations ===")
+list_a = [1, 2, 3]
+list_b = [1, 2, 3]
+list_c = list_a
+
+
+
+print("list_a == list_b :", list_a == list_b)  # same values
+print("list_a is list_b :", list_a is list_b)  # different objects
+print("list_a is list_c :", list_a is list_c)  # same object
+  ##output  
+  === Identity Operations ===
+list_a == list_b : True
+list_a is list_b : False
+list_a is list_c : True
+
+=== Code Execution Successful ===
