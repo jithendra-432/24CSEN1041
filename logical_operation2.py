3769c79c994da13ab417cf5a952de18aaa79a0fe
jithendra-432 <ddandu@student.gitam.edu>
2026-01-29 15:34:33 +0530

    Create logical_operation2.py

---
+++ logical_operation2.py
@@ -0,0 +1,21 @@
+print("=== Membership Operations ===")
+nums = [1, 2, 3, 4]
+name = "andhra"
+
+print("nums =", nums)
+print("3 in nums?       ->", 3 in nums)
+print("5  in nums?   ->", 5  in nums)
+
+print("name =", name)
+print("'dh' in name?      ->", "dh" in name)
+print("'xy'  in name?  ->", "xy"  in name)
+  ##output
+  === Membership Operations ===
+nums = [1, 2, 3, 4]
+3 in nums?       -> True
+5  in nums?   -> False
+name = andhra
+'dh' in name?      -> True
+'xy'  in name?  -> False
+
+=== Code Execution Successful ===
