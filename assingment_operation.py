4bd73300d585b1e89a63755053d5b7c7857f1d05
jithendra-432 <ddandu@student.gitam.edu>
2026-01-29 15:21:45 +0530

    Create assingment_operation.py

---
+++ assingment_operation.py
@@ -0,0 +1,36 @@
+print("=== Assignment Operations ===")
+n = 10
+print("start n =", n)
+
+n += 5      # n = n + 5
+print("n += 5  ->", n)
+
+n -= 3      # n = n - 3
+print("n -= 3  ->", n)
+
+n *= 2      # n = n * 2
+print("n *= 2  ->", n)
+
+n /= 4      # n = n / 4
+print("n /= 4  ->", n)
+
+n //= 2     # n = n // 2
+print("n //= 2 ->", n)
+
+n %= 3      # n = n % 3
+print("n %= 3  ->", n)
+
+n **= 2     # n = n ** 2
+print("n **= 2 ->", n)
+ ##output
+  === Assignment Operations ===
+start n = 10
+n += 5  -> 15
+n -= 3  -> 12
+n *= 2  -> 24
+n /= 4  -> 6.0
+n //= 2 -> 3.0
+n %= 3  -> 0.0
+n **= 2 -> 0.0
+
+=== Code Execution Successful ===
