d222b46847983aad69f7552a4776f14a3d16f8a8
jithendra-432 <ddandu@student.gitam.edu>
2026-02-04 10:55:29 +0530

    Create relation_in_strings.py

---
+++ relation_in_strings.py
@@ -0,0 +1,13 @@
+text = "abcdefghi"
+
+print("text[::2]  =", text[::2])   # step +2
+print("text[::3]  =", text[::3])   # step +3
+print("text[::-3] =", text[::-3])  # step -3
+##output
+text[::2]  = acegi
+text[::3]  = adg
+text[::-3] = ifc
+
+s4 = s1 + "  and " + s2
+ ##output
+    Concatenation (s1 + ' ' + s2): Hello and Python
