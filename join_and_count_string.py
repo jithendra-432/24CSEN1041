db9dd91b2b19e4fa2adf1de02f5e369aed8098d2
jithendra-432 <ddandu@student.gitam.edu>
2026-02-04 11:43:11 +0530

    Create join_and_count_string.py

---
+++ join_and_count_string.py
@@ -0,0 +1,26 @@
+words = ["hello", "world", "from", "Python"]
+
+joined = "_".join(words)
+print("'_'.join(words) ->", joined) 
+   ##output
+  '_'.join(words) -> hello_world_from_Python
+
+
+ sample = "I like python, python is fun"
+
+index = sample.find("python")
+print("find('python') ->", index)
+   ## output
+    find('python') -> 7
+
+
+
+
+
+  
+  sample = "hello world"
+
+result = sample.count("l")
+print("count('l') ->", result)
+##output  
+ count('l') -> 3
