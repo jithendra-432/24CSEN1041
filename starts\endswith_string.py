12dc16e55e75f01ef58799e26633f8b4035d369b
jithendra-432 <ddandu@student.gitam.edu>
2026-02-04 11:48:08 +0530

    Create starts\endswith_string.py

---
+++ starts\endswith_string.py
@@ -0,0 +1,7 @@
+sample = "   hello world   "
+
+print("startswith('   he') ->", sample.startswith("   he"))
+print("endswith('world   ') ->", sample.endswith("world   "))
+   ##output 
+  startswith('   he') -> True
+endswith('world   ') -> True
