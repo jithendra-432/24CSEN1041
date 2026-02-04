5d01ba5d49f3fc6a21bae4e90a2257631fe448ae
jithendra-432 <ddandu@student.gitam.edu>
2026-02-04 11:11:40 +0530

    Create repalce_string.py

---
+++ repalce_string.py
@@ -0,0 +1,4 @@
+sample = "I like python. python is fun."
+print("replace('python', 'Java') ->", sample.replace("python", "Java"))
+        ##output
+  replace('python', 'Java') -> I like Java. Java is fun.
