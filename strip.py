b9e758c778a619581c5cbbea3252c4bc1d1a4ca1
jithendra-432 <ddandu@student.gitam.edu>
2026-02-04 11:05:40 +0530

    Create strip.py

---
+++ strip.py
@@ -0,0 +1,11 @@
+sample1 = "  hi  "
+sample2 = "\n\thello \n"
+sample3 = "ha llo"
+
+print(sample1.strip())   # "hi"
+print(sample2.strip())   # "hello"
+print(sample3.strip())   # "ha llo"
+   ##output 
+  hi
+hello
+ha llo
