7307be7686886f17dcf2aa2b7d79235714d33aab
jithendra-432 <ddandu@student.gitam.edu>
2026-03-04 10:35:49 +0530

    Create tuple-function.py

---
+++ tuple-function.py
@@ -0,0 +1,14 @@
+t = (10, 20, 30, 40)
+
+print(len(t))   # Length
+print(max(t))   # Maximum value
+print(min(t))   # Minimum value
+print(sum(t))   # Sum of elements  ###
+
+##output
+4
+40
+10
+100
+
+=== Code Execution Successful ===
