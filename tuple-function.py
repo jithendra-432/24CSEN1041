0ef9b16f73b410e8b80524f836aac33b3b882def
jithendra-432 <ddandu@student.gitam.edu>
2026-03-04 10:37:20 +0530

    Update tuple-function.py

--- tuple-function.py
+++ tuple-function.py
@@ -12,3 +12,14 @@ print(sum(t))   # Sum of elements  ###
 100
 
 === Code Execution Successful ===
+t = (10, 20, 30, 20, 40)
+
+print(t.count(20))   # Count occurrences
+print(t.index(30))   # Find index
+
+
+output
+2
+2
+
+=== Code Execution Successful ===
