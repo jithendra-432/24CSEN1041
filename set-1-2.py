3eed823ff9c2c1fd87de5f46af278be7e676cc3d
jithendra-432 <ddandu@student.gitam.edu>
2026-03-05 14:48:46 +0530

    Create set-1-2.py

---
+++ set-1-2.py
@@ -0,0 +1,63 @@
+m1 = int(input())
+m2 = int(input())
+m3 = int(input())
+
+
+
+
+
+avg = (m1 + m2 + m3) / 3
+
+
+if avg >= 90:
+    grade = "O"
+elif avg >=80:
+    grade = "A+"
+elif avg >=70:
+    grade = "A"
+elif avg >=60:
+    grade = "B+"
+elif avg >=50:
+    grade = "B"
+elif avg >= 45:
+    grade = "C"
+elif  avg >=40:
+    grade = "P"
+else:
+    grade = "F"
+
+
+print("Average is:",round(avg,2), "Grade is", grade)
+
+
+###output
+m1 = int(input())
+m2 = int(input())
+m3 = int(input())
+
+
+
+
+
+avg = (m1 + m2 + m3) / 3
+
+
+if avg >= 90:
+    grade = "O"
+elif avg >=80:
+    grade = "A+"
+elif avg >=70:
+    grade = "A"
+elif avg >=60:
+    grade = "B+"
+elif avg >=50:
+    grade = "B"
+elif avg >= 45:
+    grade = "C"
+elif  avg >=40:
+    grade = "P"
+else:
+    grade = "F"
+
+
+print("Average is:",round(avg,2), "Grade is", grade)
