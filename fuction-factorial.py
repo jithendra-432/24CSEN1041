7226cd37c647893d237b9d65b41ef8ac0782c636
jithendra-432 <ddandu@student.gitam.edu>
2026-03-11 11:18:33 +0530

    Create fuction-factorial.py

---
+++ fuction-factorial.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = 5
+print("Factorial:", factorial(num))
+
+
+##output
+  Factorial: 120
